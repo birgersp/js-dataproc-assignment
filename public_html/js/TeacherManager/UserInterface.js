f66ad3d1616264912bd2ecabf9040a5caecdd5ba
Birger Skogeng Pedersen <birgersp@gmail.com>
2017-11-06 13:40:40 +0100

    Fixed UI, tabs now have individual content

--- public_html/js/TeacherManager/UserInterface.js
+++ public_html/js/TeacherManager/UserInterface.js
@@ -3,13 +3,20 @@ include("../thirdparty/bootstrap.min.js");
 
 TeacherManager.UserInterface = function () {
 
-    let tabContentContainer = null;
-    let tabListElement = null;
-    let initialized = false;
+    // Element which contains the user interface
+    let container = null;
 
+    // List element which contains the tab links (buttons)
+    let tabListElement = null;
+
+    // Element which contains the tab content divs
+    let tabDivsContainer = null;
+
+    let initialized = false;
     let noOfTabs = 0;
 
     function loadBootstrapCSS() {
+
         let bootstrapCSS = document.createElement("link");
         bootstrapCSS.setAttribute("rel", "stylesheet");
         bootstrapCSS.setAttribute("href", "css/bootstrap.min.css");
@@ -39,7 +46,7 @@ TeacherManager.UserInterface = function () {
         if (noOfTabs === 1)
             tabContentDiv.classList.add("active");
 
-        tabContentContainer.appendChild(tabContentDiv);
+        tabDivsContainer.appendChild(tabContentDiv);
         return tabContentDiv;
     }
 
@@ -52,18 +59,22 @@ TeacherManager.UserInterface = function () {
 
         document.body.innerHTML = "";
 
-        tabContentContainer = document.body;
+        container = document.body;
 
         tabListElement = document.createElement("ul");
         tabListElement.classList.add("nav");
         tabListElement.classList.add("nav-tabs");
-        tabContentContainer.appendChild(tabListElement);
+        container.appendChild(tabListElement);
+
+        tabDivsContainer = document.createElement("div");
+        tabDivsContainer.classList.add("tab-content");
+        container.appendChild(tabDivsContainer);
 
         let teachersDiv = createTab("Teacher Workload");
-        teachersDiv.innerHTML = "(TEACHER WORKLOAD)";
+        teachersDiv.innerHTML = "<h4>Teacher Workload</h4>";
 
         let coursesDiv = createTab("Courses");
-        coursesDiv.innerHTML = "(COURSES)";
+        coursesDiv.innerHTML = "<h4>Courses</h4>";
 
         initialized = true;
     };
