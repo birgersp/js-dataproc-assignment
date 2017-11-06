5e01036f2bf0e495e6c60cdc3a001a4edebafe39
Birger <birgersp@gmail.com>
2017-11-06 11:14:33 +0100

    Moved third-party library inclusions to user interface script

--- public_html/js/TeacherManager/UserInterface.js
+++ public_html/js/TeacherManager/UserInterface.js
@@ -1,6 +1,10 @@
+include("../thirdparty/jquery-3.2.1.min.js");
+include("../thirdparty/bootstrap.min.js");
+
 TeacherManager.UserInterface = function() {
 
     function createTab(label) {
+
 
     }
 
--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,9 +1,5 @@
 "use strict";
 
-// Libraries
-include("thirdparty/jquery-3.2.1.min.js");
-include("thirdparty/bootstrap.min.js");
-
 include("TeacherManager/core.js");
 include("TeacherManager/UserInterface.js");
 include("TeacherManager/Data.js");
