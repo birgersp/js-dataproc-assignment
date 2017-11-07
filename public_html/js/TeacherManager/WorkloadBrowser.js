d4e71c62d65601a7f701017a777fa940d7523e53
Birger Skogeng Pedersen <birgersp@gmail.com>
2017-11-07 10:39:31 +0100

    Styling chart WIP

--- public_html/js/TeacherManager/WorkloadBrowser.js
+++ public_html/js/TeacherManager/WorkloadBrowser.js
@@ -13,9 +13,17 @@ TeacherManager.WorkloadBrowser = function() {
     this.container = null;
     this.teachers = null;
 
+    let chart = null;
+
+    function onChartClick(event, array) {
+
+        // TODO: appropriately handle clicks
+        // to handle clicks on labels, see: https://stackoverflow.com/questions/39001705/chart-js-click-on-labels-using-bar-chart
+    }
+
     this.initialize = function() {
 
-        self.container.innerHTML = "";
+        self.container.innerHTML = "<input type=\"checkbox\"></input>";
 
         let canvasContainer = createElement("div", self.container);
         canvasContainer.style.setProperty("width", "1000px");
@@ -23,36 +31,33 @@ TeacherManager.WorkloadBrowser = function() {
         let canvas = createElement("canvas", canvasContainer);
         let style = canvas.style;
 
-        var myChart = new Chart(canvas, {
+        chart = new Chart(canvas, {
             type: 'bar',
             data: {
-                // Name of teachers
+                // Name of each teacher
                 labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-                datasets: [{
-                        data: [12, 19, 3, 5, 2, 3],
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
-                        ],
-                        borderColor: [
-                            'rgba(255,99,132,1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
-                        ],
+                datasets: [
+                    {
+                        label: 'Spring',
+
+                        // Workload of each teacher during the spring
+                        data: [19, 3],
+                        backgroundColor: 'rgba(255, 206, 86, 0.2)',
+                        borderColor: 'rgba(255, 206, 86, 1)',
+                        borderWidth: 1
+                    },
+                    {
+                        label: 'Fall',
+
+                        // Workload of each teacher during the fall
+                        data: [12, 5],
+                        backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                        borderColor: 'rgba(54, 162, 235, 1)',
                         borderWidth: 1
                     }]
             },
             options: {
-                legend: {
-                    display: false
-                },
+                onClick: onChartClick,
                 scales: {
                     yAxes: [{
                             ticks: {
