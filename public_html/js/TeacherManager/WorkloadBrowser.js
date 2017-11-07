67045c350a9f96ef85287b216f420d795843f4da
Birger Skogeng Pedersen <birgersp@gmail.com>
2017-11-07 13:01:08 +0100

    Added fall chart, fixed color values

--- public_html/js/TeacherManager/WorkloadBrowser.js
+++ public_html/js/TeacherManager/WorkloadBrowser.js
@@ -9,17 +9,17 @@ if (!window.TeacherManager)
 
 TeacherManager.WorkloadBrowser = function() {
 
+    const COLOR_ALPHA = 0.2;
+    const MAX_WORKLOAD_THRESHOLD = 250;
     let self = this;
 
     this.container = null;
 
     function onChartClick(event, array) {
-
     }
 
     this.initialize = function() {
-
-
+        // TODO: maybe show some warning here, "awating data"
     };
 
     this.setTeacherData = function(teachers) {
@@ -29,7 +29,8 @@ TeacherManager.WorkloadBrowser = function() {
         let canvasContainer = createElement("div", self.container);
         canvasContainer.style.setProperty("max-width", "1600px");
 
-        let canvas = createElement("canvas", canvasContainer);
+        let springCanvas = createElement("canvas", canvasContainer);
+        let fallCanvas = createElement("canvas", canvasContainer);
 
         let teacherNames = [];
 
@@ -37,14 +38,34 @@ TeacherManager.WorkloadBrowser = function() {
         let springWorkloadColors = [];
         let springWorkloadBorderColors = [];
 
+        let fallWorkloadDataValues = [];
+        let fallWorkloadColors = [];
+        let fallWorkloadBorderColors = [];
+
         function getWorkloadColorRGB(workloadPercent) {
 
-            let hue = 1 / 3;
+            let factor;
 
-            let rgb = colorconv.HSV2RGB([hue, 1, 1]);
-            console.log(rgb);
+            if (workloadPercent <= 100) {
+                factor = workloadPercent / 100;
+            } else {
+                let overshoot = workloadPercent - 100;
+                factor = 1 - overshoot / (MAX_WORKLOAD_THRESHOLD - 100);
+            }
 
-            let rgbString = "" + rgb;
+            if (factor < 0)
+                factor = 0;
+
+            let hue = 1 / 3 * factor;
+            let rgb = colorconv.HSV2RGB([hue, 1, 0.85]);
+            let rgbString = "";
+
+            for (let i in rgb) {
+                if (i > 0)
+                    rgbString += ",";
+                rgbString += Math.floor(rgb[i]);
+            }
+
             return rgbString;
         }
 
@@ -69,48 +90,70 @@ TeacherManager.WorkloadBrowser = function() {
 
             teacherNames.push(teacherName);
 
-            let workloadPercent = teacher.workloadPercent.spring;
+            let springWorkload = teacher.workloadPercent.spring;
 
-            springWorkloadColors.push(getWorkloadColor(workloadPercent));
-            springWorkloadBorderColors.push(getWorkloadBorderColor(workloadPercent));
+            springWorkloadColors.push(getWorkloadColor(springWorkload));
+            springWorkloadBorderColors.push(getWorkloadBorderColor(springWorkload));
+            springWorkloadDataValues.push(springWorkload);
 
-            springWorkloadDataValues.push(workloadPercent);
+            let fallWorkload = teacher.workloadPercent.fall;
+
+            fallWorkloadColors.push(getWorkloadColor(fallWorkload));
+            fallWorkloadBorderColors.push(getWorkloadBorderColor(fallWorkload));
+            fallWorkloadDataValues.push(fallWorkload);
+
         }
 
-        let chartOptions = {
-            onClick: onChartClick(),
-            scales: {
-                xAxes: [{
-                        ticks: {
-                            beginAtZero: true,
-                            callback: function(value, index, values) {
-                                return value + "%";
-                            }
-                        }
-                    }]
-            },
-            legend: {
-                display: false
-            },
-            title: {
-                display: true,
-                text: "Workload, Spring"
-            }
-        };
+        function createChart(canvas, title, dataValues, dataColors, dataBorderColors) {
+            new Chart(canvas, {
+                type: 'horizontalBar',
+                data: {
+                    labels: teacherNames,
+                    datasets: [
+                        {
+                            data: dataValues,
+                            backgroundColor: dataColors,
+                            borderColor: dataBorderColors,
+                            borderWidth: 1
+                        }]
+                },
+                options: {
+                    onClick: onChartClick,
+                    scales: {
+                        xAxes: [{
+                                ticks: {
+                                    beginAtZero: true,
+                                    callback: function(value, index, values) {
+                                        return value + "%";
+                                    }
+                                }
+                            }]
+                    },
+                    legend: {
+                        display: false
+                    },
+                    title: {
+                        display: true,
+                        text: title
+                    }
+                }
+            });
+        }
 
-        new Chart(canvas, {
-            type: 'horizontalBar',
-            data: {
-                labels: teacherNames,
-                datasets: [
-                    {
-                        data: springWorkloadDataValues,
-                        backgroundColor: springWorkloadColors,
-                        borderColor: springWorkloadBorderColors,
-                        borderWidth: 1
-                    }]
-            },
-            options: chartOptions
-        });
+        createChart(
+                springCanvas,
+                "Workload, Spring",
+                springWorkloadDataValues,
+                springWorkloadColors,
+                springWorkloadBorderColors
+                );
+
+        createChart(
+                fallCanvas,
+                "Workload, Fall",
+                fallWorkloadDataValues,
+                fallWorkloadColors,
+                fallWorkloadBorderColors
+                );
     };
 };
