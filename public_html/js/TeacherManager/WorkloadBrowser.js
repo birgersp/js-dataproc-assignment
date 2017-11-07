aaca44b6fafd8e403c22a76610edc11dae93cfd6
Birger Skogeng Pedersen <birgersp@gmail.com>
2017-11-07 09:40:05 +0100

    Added a chart to workload browser

--- public_html/js/TeacherManager/WorkloadBrowser.js
+++ public_html/js/TeacherManager/WorkloadBrowser.js
@@ -1,5 +1,8 @@
 "use strict";
 
+include("../utilities/utilities.js");
+include("../thirdparty/Chart.min.js");
+
 if (!window.TeacherManager)
     window.TeacherManager = {};
 
@@ -12,8 +15,53 @@ TeacherManager.WorkloadBrowser = function() {
 
     this.initialize = function() {
 
-        self.container.innerHTML = "Initializing";
-//        let canvas = 
+        self.container.innerHTML = "";
+
+        let canvasContainer = createElement("div", self.container);
+        canvasContainer.style.setProperty("width", "1000px");
+
+        let canvas = createElement("canvas", canvasContainer);
+        let style = canvas.style;
+
+        var myChart = new Chart(canvas, {
+            type: 'bar',
+            data: {
+                // Name of teachers
+                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                datasets: [{
+                        data: [12, 19, 3, 5, 2, 3],
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(255,99,132,1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ],
+                        borderWidth: 1
+                    }]
+            },
+            options: {
+                legend: {
+                    display: false
+                },
+                scales: {
+                    yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                }
+            }
+        });
     };
 
     this.update = function() {
