13301f40542b5785b956b25c6cd1c4e2e30c6892
Birger Skogeng Pedersen <birgersp@gmail.com>
2017-11-07 14:10:33 +0100

    Now listing teachers in teacher browser

--- public_html/js/TeacherManager/TeacherBrowser.js
+++ public_html/js/TeacherManager/TeacherBrowser.js
@@ -3,12 +3,43 @@ if (!window.TeacherManager)
 
 TeacherManager.TeacherBrowser = function() {
 
+    let self = this;
+
+    let attributeKeys = {
+        lastName: "Lastname",
+        firstName: "Firstname",
+        facultyCode: "Faculty",
+        departmentCode: "Department",
+        isExternal: "Is external",
+        isStudentAssistant: "Is stud.ass.",
+        employmentPercentage: "% Employment"
+    };
+
     this.container = null;
 
     this.initialize = function() {
     };
 
     this.setTeacherData = function(teachers) {
+
         this.container.innerHTML = "";
+
+        let table = createElement("table", self.container, {
+            "class": "table table-striped"
+        });
+        let tableHead = createElement("thead", table);
+        let headRow = createElement("tr", tableHead);
+
+        for (let key in attributeKeys)
+            createElement("th", headRow, {innerHTML: attributeKeys[key]});
+
+        let tableBody = createElement("tbody", table);
+
+        for (let teacherID in teachers) {
+            let teacher = teachers[teacherID];
+            let tableBodyRow = createElement("tr", tableBody);
+            for (let key in attributeKeys)
+                createElement("td", tableBodyRow, {innerHTML: teacher[key]});
+        }
     };
 };
